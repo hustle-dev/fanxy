f0aab0abe7e422d61408cc7775fa951d1f7b6d32
hustle-dev <dlwoabsdk@gmail.com>
2025-07-05 17:29:03 +0900

    [feat] Header 컴포넌트에 현재 경로 기반 Link 스타일링

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { Moon, Sun } from 'lucide-react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { useTheme } from 'next-themes';
 
 import cn from '~/app/utils/cn';
@@ -9,14 +10,19 @@ import cn from '~/app/utils/cn';
 import Button from './Button';
 
 const Header = () => {
+  const pathname = usePathname();
   const { theme, setTheme } = useTheme();
   const isDark = theme === 'dark';
+  const isHome = pathname === '/';
   return (
     <header className="mx-auto my-6 flex w-full max-w-3xl flex-1 justify-between px-6">
       <nav className="flex items-center">
         <Link
           href="/"
-          className="text-muted-foreground hover:text-foreground transition-colors"
+          className={cn(
+            'text-muted-foreground hover:text-foreground transition-colors',
+            { 'text-foreground': isHome },
+          )}
         >
           Home
         </Link>
@@ -34,14 +40,14 @@ const Header = () => {
             'text-muted-foreground group-hover:text-foreground h-[1rem] w-[1rem] scale-100 rotate-0 transition-all',
             { 'scale-0 -rotate-90': isDark },
           )}
-          suppressHydrationWarning={true}
+          suppressHydrationWarning
         />
         <Moon
           className={cn(
             'text-muted-foreground group-hover:text-foreground absolute h-[1rem] w-[1rem] scale-0 rotate-90 transition-all',
             { 'scale-100 rotate-0': isDark },
           )}
-          suppressHydrationWarning={true}
+          suppressHydrationWarning
         />
         <span className="sr-only">테마 전환</span>
       </Button>
